72c95b592031d002423a6d3433cdbda54262ff0d
Oumaima Gouajla <oumaima.gouajla@etudiant.univ-rennes1.fr>
2024-04-28 11:33:13 +0200

    Q8 - pipe done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { FilterPokemonPipePipe } from './filter-pokemon--pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponentComponent,
+    FilterPokemonPipePipe
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
